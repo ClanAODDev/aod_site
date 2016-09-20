76f166e2e7af3c805cd5a6cb43579bcd2cedf006
Chris Deaton <dcdeaton@ncsu.edu>
2016-09-20 00:16:13 -0400

    start work on landing page template - add museo font - add grid assets, start template - add stylesheet

---
+++ index.php
@@ -0,0 +1,20 @@
+<div class="hero-video">
+
+	<video id="video" poster="" autoplay loop muted>
+		<source src="http://storage.googleapis.com/video.wixstatic.com/video/45f29b_df5d09c8540c4f3285e90d7f9a456575/1080p/mp4/file.mp4" type="video/mp4">
+		</video>
+
+		<div class="grid"></div>
+
+		<div class="hero-text">
+			<h2>
+				Looking for a gaming community?
+			</h2>
+			<p>
+				Welcome home.
+			</p>
+		</div>
+
+	</div>
+
+	<div class="video-overlay"></div>
