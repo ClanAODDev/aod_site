0f49bfbcb54f5aacfd6b7a3063fd9bf7fd76811b
Chris Deaton <dcdeaton@ncsu.edu>
2016-09-24 10:55:12 -0400

    initial work on hero image

--- index.php
+++ index.php
@@ -1,20 +1,27 @@
+<html>
+<head>
+    <title>ClanAOD.net</title>
+    <link href="style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+
 <div class="hero-video">
+    <div class="grid"></div>
 
-	<video id="video" poster="" autoplay loop muted>
-		<source src="http://storage.googleapis.com/video.wixstatic.com/video/45f29b_df5d09c8540c4f3285e90d7f9a456575/1080p/mp4/file.mp4" type="video/mp4">
-		</video>
+    <video id="video" poster="" autoplay loop muted>
+        <source
+            src="http://storage.googleapis.com/video.wixstatic.com/video/45f29b_df5d09c8540c4f3285e90d7f9a456575/1080p/mp4/file.mp4"
+            type="video/mp4">
+    </video>
 
-		<div class="grid"></div>
+    <div class="hero-text">
+        <h2>Looking for a gaming community?</h2>
+        <p>Welcome home.</p>
+    </div>
 
-		<div class="hero-text">
-			<h2>
-				Looking for a gaming community?
-			</h2>
-			<p>
-				Welcome home.
-			</p>
-		</div>
+</div>
 
-	</div>
+<div class="video-overlay"></div>
 
-	<div class="video-overlay"></div>
+</body>
+</html>
