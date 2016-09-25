121ca96aa72e6d4d2d74b77e3d872ee88d106ab4
Chris Deaton <dcdeaton@ncsu.edu>
2016-09-25 16:27:33 -0400

    implement stylus, add gulp task runner

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minifycss = require('gulp-minify-css');
+var stylus = require('gulp-stylus');
+
+
+gulp.task('styles', function(){
+    gulp.src(['assets/stylus/main.styl'])
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }}))
+        .pipe(stylus())
+        .pipe(minifycss())
+        .pipe(gulp.dest('public/css/'))
+});
+
+gulp.task('scripts', function(){
+    return gulp.src('assets/js/**/*.js')
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }}))
+        .pipe(concat('main.js'))
+        .pipe(babel())
+        .pipe(gulp.dest('public/js/'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/js/'))
+});
+
+gulp.task('default', function(){
+    gulp.watch("assets/stylus/**/*.styl", ['styles']);
+    gulp.watch("assets/js/**/*.js", ['scripts']);
+});
