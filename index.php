8590f02cd2c954fa015845949cfc9221dcefead4
Chris Deaton <dcdeaton@ncsu.edu>
2016-09-25 15:42:10 -0400

    clean up landing page hero image

--- index.php
+++ index.php
@@ -6,24 +6,53 @@
 <body>
 
 <div class="hero-video">
-    <div class="grid"></div>
-
-    <video id="video" poster="" autoplay loop muted>
-        <source
-            src="http://storage.googleapis.com/video.wixstatic.com/video/45f29b_df5d09c8540c4f3285e90d7f9a456575/1080p/mp4/file.mp4"
-            type="video/mp4">
-    </video>
-
-    <div class="hero-text">
-        <img src="./public/images/official-logo.png" class="aod-logo" />
-
-        <h1>Looking for a gaming community?</h1>
-        <h2 class="subtitle">Welcome home.</h2>
+    <div class="video-container">
+        <div class="hero-text">
+            <img src="./public/images/official-logo.png" class="aod-logo"/>
+            <h1>Looking for a gaming community?</h1>
+            <h2 class="subtitle">Welcome home.</h2>
+        </div>
+        <div class="grid"></div>
+        <video id="video" poster="" autoplay loop muted>
+            <source
+                src="http://storage.googleapis.com/video.wixstatic.com/video/45f29b_df5d09c8540c4f3285e90d7f9a456575/1080p/mp4/file.mp4"
+                type="video/mp4">
+        </video>
     </div>
 
 </div>
 
-<div class="video-overlay"></div>
+<section>
+    <h1>Testing Section</h1>
+    <h1>HTML Ipsum Presents</h1>
+
+    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas.
+        Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
+        egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et
+        sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet,
+        wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac
+        dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+
+    <h2>Header Level 2</h2>
+
+    <ol>
+        <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+        <li>Aliquam tincidunt mauris eu risus.</li>
+    </ol>
+
+    <blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet
+            congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis,
+            tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p>
+    </blockquote>
+
+    <h3>Header Level 3</h3>
+
+    <ul>
+        <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+        <li>Aliquam tincidunt mauris eu risus.</li>
+    </ul>
+
+</section>
 
 </body>
 </html>
