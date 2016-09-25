1e88da4a9c0e33bdb2f79dd80aaee33435b7738c
Chris Deaton <dcdeaton@ncsu.edu>
2016-09-25 19:51:54 -0400

    add navbar styling

--- index.php
+++ index.php
@@ -22,35 +22,41 @@
 
 </div>
 
-<section>
-    <h1>Testing Section</h1>
-    <h1>HTML Ipsum Presents</h1>
+<div class="primary-nav">
+    <div class="nav-texture">
+        <ul>
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Forums</a></li>
+            <li><a href="#">Divisions</a></li>
+            <li><a href="#">History</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Apply</a></li>
+        </ul>
+    </div>
+</div>
 
-    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas.
-        Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
-        egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et
-        sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet,
-        wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac
-        dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+<section class="supported-games">
+    <h1>18 Supported Games and Over 3000 Active Members</h1>
+    <p>There's no shortage of excitement when you have fun, engaging, and friendly members to join in on the action with you. Work as a team to defeat the enemy, and make great friends all at the same time.</p>
 
-    <h2>Header Level 2</h2>
-
-    <ol>
-        <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-        <li>Aliquam tincidunt mauris eu risus.</li>
-    </ol>
-
-    <blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet
-            congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis,
-            tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p>
-    </blockquote>
-
-    <h3>Header Level 3</h3>
-
-    <ul>
-        <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-        <li>Aliquam tincidunt mauris eu risus.</li>
-    </ul>
+    <div class="games-listing">
+        <ul>
+            <li>AODR</li>
+            <li>ARK</li>
+            <li>Armored Warfare</li>
+            <li>Battlefield</li>
+            <li>Battlefront</li>
+            <li>Black Desert</li>
+            <li>Jedi Knight/li>
+            <li>Overwatch</li>
+            <li>Planetside 2</li>
+            <li>Skyforge</li>
+            <li>Tom Clancy</li>
+            <li>Warframe</li>
+            <li>War Thunder</li>
+            <li>World of Warcraft</li>
+        </ul>
+    </div>
 
 </section>
 
