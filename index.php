18b6a5fb205620b09fc87aae2d83ab575cc84bdf
Chris Deaton <dcdeaton@ncsu.edu>
2016-09-28 19:57:11 -0400

    add sections to landing page, style sections

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 <div class="primary-nav">
     <div class="nav-texture">
         <ul>
-            <li class="active"><a href="#">Home</a><img src="public/images/logo-mini.png" /></li>
+            <li class="active"><a href="#">Home</a><img src="public/images/logo-mini.png"/></li>
             <li><a href="#">Forums</a></li>
             <li><a href="#">Divisions</a></li>
             <li><a href="#">History</a></li>
@@ -37,29 +37,69 @@
 
 <section class="supported-games">
     <div class="content-container centered">
-        <h1><strong>18</strong> Supported Games<br />and Over <strong>3000</strong> Active Members</h1>
+        <h1><strong>18</strong> Supported Games<br/>and Over <strong>3000</strong> Active Members</h1>
         <p>There's no shortage of excitement when you have fun, engaging, and friendly members to join in on the action
             with you. Work as a team to defeat the enemy, and make great friends all at the same time.</p>
 
-            <ul class="games-listing">
-                <li class="game aodr">AOD Racing</li>
-                <li class="game ark">ARK</li>
-                <li class="game aw">Armored Warfare</li>
-                <li class="game bf">Battlefield</li>
-                <li class="game swb">Battlefront</li>
-                <li class="game bd">Black Desert</li>
-                <li class="game jk">Jedi Knight/li>
-                <li class="game ow">Overwatch</li>
-                <li class="game ps2">Planetside 2</li>
-                <li class="game sf">Skyforge</li>
-                <li class="game tc">Tom Clancy</li>
-                <li class="game wf">Warframe</li>
-                <li class="game wt">War Thunder</li>
-                <li class="game wow">World of Warcraft</li>
-            </ul>
-
+        <ul class="games-listing">
+            <li class="game aodr">AOD Racing</li>
+            <li class="game ark">ARK</li>
+            <li class="game aw">Armored Warfare</li>
+            <li class="game bf">Battlefield</li>
+            <li class="game swb">Battlefront</li>
+            <li class="game bd">Black Desert</li>
+            <li class="game jk">Jedi Knight/li>
+            <li class="game ow">Overwatch</li>
+            <li class="game ps2">Planetside 2</li>
+            <li class="game sf">Skyforge</li>
+            <li class="game tc">Tom Clancy</li>
+            <li class="game wf">Warframe</li>
+            <li class="game wt">War Thunder</li>
+            <li class="game wow">World of Warcraft</li>
+        </ul>
     </div>
+</section>
 
+<section class="founded-info">
+    <div class="content-container">
+        <h1>Founded in <strong>1999</strong><br/>And Still Kicking!</h1>
+        <p class="short-width">The Angels of Death is a time-tested organization, supporting over 56 major gaming titles
+            in the past 15 years including classics like Medal of Honor: Allied Assault, and Swat 3.</p>
+        <p class="short-width">We've lasted this long because of the tireless efforts of people who love gaming, and our
+            community is as diverse as the games we play. AOD is truly a family, and we've never stopped growing.</p>
+        <p class="short-width"><a href="#">Read the history of AOD</a></p>
+    </div>
+</section>
+
+<section class="social-media">
+    <div class="content-container centered">
+        <h1>Catch up with us on social media</h1>
+        <p>We are constantly working to reach out to gamers like you all over the world! Connect with us on a variety of
+            social media to see what we're up to!</p>
+        <ul class="social-media-sites">
+            <li>Twitch</li>
+            <li>Twitter</li>
+            <li>Steam</li>
+            <li>Youtube</li>
+        </ul>
+    </div>
+</section>
+
+<section class="honor-info">
+    <div class="content-container">
+        <h1>The Angels of Death is<br/>an honor clan</h1>
+        <p>Chief among our concerns is fair play, respect among members, and courtesy to our volunteer staff leadership.
+            These core values contribute to an atmosphere that anyone can enjoy.</p>
+        <p><a href="#">Read our Code of Conduct</a></p>
+    </div>
+</section>
+
+<section class="apply-info">
+    <div class="content-container centered">
+        <h1>So what are you waiting for?</h1>
+        <p>Complete a clan application with one of our divisions, and see if we're a good fit for each other.</p>
+        <p><a href="#" class="apply-button">Apply</a></p>
+    </div>
 </section>
 
 </body>
