cb19f95fcb71e7e7a5940193de17f432084fffe5
Chris Deaton <dcdeaton@ncsu.edu>
2016-10-05 09:30:52 -0400

    add compiled assets

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+var ClanAOD = ClanAOD || {};
+
+(function ($) {
+    ClanAOD = {
+        setup: function () {
+            var scrollLocation = 700; //px
+            $(window).bind('scroll', function () {
+                if ($(window).scrollTop() > scrollLocation) {
+                    $('.home').addClass('show-logo').closest('.primary-nav').addClass('fixed');
+                } else {
+                    $('.home').removeClass('show-logo').closest('.primary-nav').removeClass('fixed');
+                }
+            });
+        }
+    }
+})(jQuery);
+
+ClanAOD.setup();
