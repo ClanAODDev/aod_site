94b5efbc712f41ce2b5586a704be64307f72525f
Chris Deaton <dcdeaton@ncsu.edu>
2016-10-06 10:27:12 -0400

    fix path for navbar logo

--- assets/partials/navbar.php
+++ assets/partials/navbar.php
@@ -1,7 +1,7 @@
 <div class="primary-nav">
     <div class="nav-texture">
         <ul>
-            <li class="home"><a href="#">Home</a><img src="../../public/images/logo-mini.png"/></li>
+            <li class="home"><a href="#">Home</a><img src="public/images/logo-mini.png"/></li>
             <li><a href="#">Forums</a></li>
             <li><a href="#">Divisions</a></li>
             <li><a href="#">History</a></li>
