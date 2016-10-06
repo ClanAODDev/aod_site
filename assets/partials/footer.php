465a0194e9f896082565845812d4e4785b1107f5
Chris Deaton <dcdeaton@ncsu.edu>
2016-10-06 11:52:18 -0400

    minor refactoring

--- assets/partials/footer.php
+++ assets/partials/footer.php
@@ -90,4 +90,4 @@
 </section>
 
 <script src="http://code.jquery.com/jquery-2.2.4.min.js"></script>
-<script src="../../public/js/main.js"></script>
+<script src="./public/js/main.js"></script>
--- divisions.php
+++ divisions.php
@@ -2,29 +2,14 @@
 
 require('header.php');
 require('./assets/partials/navbar.php');
-
-$divisions = [
-    ['name' => 'Battlefield', 'members' => 409, 'abbreviation' => 'bf'],
-    ['name' => 'Battlefront', 'members' => 198, 'abbreviation' => 'swb'],
-    ['name' => 'Black Desert Online', 'members' => 198, 'abbreviation' => 'bdo'],
-    ['name' => 'Planetside 2', 'members' => 512, 'abbreviation' => 'ps2'],
-    ['name' => 'Warframe', 'members' => 274, 'abbreviation' => 'wf'],
-    ['name' => 'Tom Clancy', 'members' => 167, 'abbreviation' => 'tc'],
-    ['name' => 'Jedi Knight', 'members' => 167, 'abbreviation' => 'jk'],
-    ['name' => 'Overwatch', 'members' => 238, 'abbreviation' => 'ow'],
-    ['name' => 'Warcraft', 'members' => 69, 'abbreviation' => 'wow'],
-];
+require ('./data/divisionCollection.php')
 
 ?>
 
 <section class="lobby-hero">
     <div class="content-container">
         <h1>Gaming Divisions</h1>
-        <p class="short-width">Divisions are the lifeblood of the Angels of Death. As such, the leadership invests a
-            great deal of
-            effort
-            and time into vetting each individual game for its worthiness for inclusion as an active division in the
-            community.</p>
+        <p class="short-width">Divisions are the lifeblood of the Angels of Death. As such, the leadership invests a great deal of effort and time into vetting each individual game for its worthiness for inclusion as an active division in the community.</p>
 
         <div class="divisions-list">
             <div class="collection">
