05b8062a2f92a08c68017d3ab40cb6a6e3d2e34a
Chris Deaton <dcdeaton@ncsu.edu>
2016-10-23 14:01:44 -0400

    update supported games model, add ignore for videos

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php get_header() ?>
 
-<!--    <div class="modal">
-        <div class="intro-video">
-            <iframe src="https://www.youtube.com/embed/2M7whdISquM" frameborder="0" height="75%" width="75%" allowfullscreen></iframe>
-        </div>
-        <div class="modal-overlay"></div>
-    </div>-->
+    <!--    <div class="modal">
+            <div class="intro-video">
+                <iframe src="https://www.youtube.com/embed/2M7whdISquM" frameborder="0" height="75%" width="75%" allowfullscreen></iframe>
+            </div>
+            <div class="modal-overlay"></div>
+        </div>-->
 
     <div class="hero-video">
         <div class="video-container">
@@ -20,9 +20,8 @@
             <div class="grid"></div>
 
             <video id="video" poster="<?= get_template_directory_uri() ?>/public/images/video-poster.jpg" autoplay loop muted>
-                <source
-                    src="http://storage.googleapis.com/video.wixstatic.com/video/45f29b_df5d09c8540c4f3285e90d7f9a456575/1080p/mp4/file.mp4"
-                    type="video/mp4">
+                <source src="<?= get_template_directory_uri() ?>/public/videos/hero-video.mp4"
+                        type="video/mp4">
             </video>
         </div>
     </div>
