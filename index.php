ae037c1cbdc0d49f296eb4aaa8e7f6da2e7be4b2
Chris Deaton <dcdeaton@ncsu.edu>
2016-12-23 11:50:32 -0500

    fix intro video height

--- index.php
+++ index.php
@@ -7,13 +7,10 @@
 get_header();
 
 ?>
-
-
     <div class="intro-video">
         <iframe src="https://www.youtube.com/embed/ZCpfySvao9s?autoplay=0&showinfo=0&enablejsapi=1"
-                id="video-iframe" frameborder="0" width="960px" height="750px" allowfullscreen></iframe>
+                id="video-iframe" frameborder="0" width="960px" height="650px" allowfullscreen></iframe>
     </div>
-
 
     <div class="hero-video">
         <div class="video-container">
