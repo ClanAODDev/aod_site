8ba11702504a89f08c0523344df179fa1d2b899a
Chris Deaton <dcdeaton@ncsu.edu>
2016-12-28 14:33:33 -0500

    fix momentary style flash

--- assets/partials/introVideo.php
+++ assets/partials/introVideo.php
@@ -1,4 +1,4 @@
-<div class="intro-video">
+<div class="intro-video" style="display: none;">
     <iframe src="https://www.youtube.com/embed/ZCpfySvao9s?autoplay=0&amp;showinfo=0&amp;enablejsapi=1"
             id="video-iframe" frameborder="0" width="960px" height="650px" allowfullscreen></iframe>
 </div>
