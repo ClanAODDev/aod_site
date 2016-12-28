5ade795052eaba20652d39a4b043abbc8783fa88
Chris Deaton <dcdeaton@ncsu.edu>
2016-12-28 17:46:04 -0500

    versionize stylesheet

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <title>ClanAOD.net</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/style.css" />
+    <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/style.css?v=0.1.1" />
 
     <script src="<?=get_template_directory_uri() . '/assets/js/modernizr.js' ?>"></script>
 
