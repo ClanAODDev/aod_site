1c3d7928d7a30f73f47743a02747b3ea391e2bdd
Chris Deaton <dcdeaton@ncsu.edu>
2016-12-28 14:12:16 -0500

    update css, apply modal

--- assets/forms/applyForm.php
+++ assets/forms/applyForm.php
@@ -5,18 +5,12 @@ use ClanAOD\Repositories\DivisionRepository;
 $divisions = DivisionRepository::allDivisions(); ?>
 
 <div class="apply-form">
-    <div class="form-container" style="
-        background-color: #111;
-        border: solid 4px #555;
-        border-radius: 4px;
-        text-align: center;
-    ">
-        <h2>Apply for a division</h2>
-        <ul class="games-listing">
-            <?php foreach ($divisions as $post): setup_postdata($post); ?>
-                <li class="game <?= meta('abbreviation'); ?>"></li>
-            <?php endforeach;
-            wp_reset_postdata(); ?>
-        </ul>
-    </div>
+    <h2>Apply for a division</h2>
+    <ul class="games-listing">
+        <?php foreach ($divisions as $post): setup_postdata($post); ?>
+            <li class="game <?= meta('abbreviation'); ?>"></li>
+        <?php endforeach;
+        wp_reset_postdata(); ?>
+    </ul>
 </div>
+
