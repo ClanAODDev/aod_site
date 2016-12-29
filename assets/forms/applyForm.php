e22b13a0de47690c66ff1b3f6103c5181869bf73
Chris Deaton <dcdeaton@ncsu.edu>
2016-12-28 22:10:05 -0500

    add tooltip

--- assets/forms/applyForm.php
+++ assets/forms/applyForm.php
@@ -6,17 +6,18 @@ $divisions = DivisionRepository::allDivisions(); ?>
 
 <div class="apply-form" style="display: none;">
     <h2>Apply for a division</h2>
-    <hr />
+    <hr/>
     <div class="games-listing">
         <?php foreach ($divisions as $post): setup_postdata($post); ?>
             <a href="#" data-application-id="<?= meta('application_id') ?>"
-               data-application-link>
+               data-application-link class="tooltips">
                 <img class="game" src="<?= meta('division_icon'); ?>"/>
+                <span><?= $post->post_title ?></span>
             </a>
         <?php endforeach;
         wp_reset_postdata(); ?>
     </div>
-    <hr />
+    <hr/>
     <p>You will be taken to our forums to complete the application process.</p>
 </div>
 
