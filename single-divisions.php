14de9d448be74e2b46a7c3c91d4ba0103fc63cae
Chris Deaton <dcdeaton@ncsu.edu>
2016-12-29 01:09:32 -0500

    add division header image meta

--- single-divisions.php
+++ single-divisions.php
@@ -1,23 +1,20 @@
 <?php
 get_header();
 
-use ClanAOD\Helpers;
-
 /**
  * Template Name: Divisions Template
  */
 
-$abbrev = meta('abbreviation');
 $url = get_template_directory_uri() . '/public/images/';
-$divisionBg = "url('{$url}division-headers/{$abbrev}.jpg')";
 $borderBg = "url('{$url}division-bg-border.jpg')";
 
 include(get_template_directory() . '/assets/partials/navbar.php'); ?>
 
 <?php if (have_posts()): while (have_posts()): the_post(); ?>
+    <?php $background = meta('header_image'); ?>
 
     <section class="division" data-application-id="<?= meta('application_id') ?>"
-             style="background: <?= "{$divisionBg} no-repeat, {$borderBg} repeat-x" ?>; background-position: top center;">
+             style="background: <?= "url({$background}) no-repeat, {$borderBg} repeat-x" ?>; background-position: top center;">
         <div class="section-content-container">
             <div id="sub-nav"></div>
 
