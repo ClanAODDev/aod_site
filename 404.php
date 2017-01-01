3d61a86a6cc6a3db087a7d7334c40b0034ab1b80
Chris Deaton <dcdeaton@ncsu.edu>
2017-01-01 16:42:52 -0500

    add 404, add menu item class

--- 404.php
+++ 404.php
@@ -19,6 +19,10 @@ get_header();
         justify-content: center;
     }
 
+    h1 {
+        font-size: 3em;
+    }
+
     .wrap {
         display: flex; /* or inline-flex */
         flex-direction: column; /* or usually or row */
@@ -37,5 +41,6 @@ get_header();
 
 <div class="wrap" style="background: url('<?= get_template_directory_uri() ?>/public/images/broke.jpg') no-repeat;">
     <h1>404</h1>
-    <p><strong>Kid_A</strong> must have broken something...</p>
+    <p>It looks like that page doesn't exist. </p>
+    <p><a href="/forums" class="call-to-action-button is-small">Go Home</a></p>
 </div>
