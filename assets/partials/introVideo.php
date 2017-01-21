51772c205109c6892192c38d43154146bc8e368d
Chris Deaton <dcdeaton@ncsu.edu>
2017-01-21 17:25:49 -0500

    fix intro video close button

--- assets/partials/introVideo.php
+++ assets/partials/introVideo.php
@@ -1,8 +1,7 @@
 <?php defined('ABSPATH') or die(); ?>
 
 <div class="intro-video" style="display: none;">
-    <div class="close-video"
-         style="position: absolute; top: 0; right: 0; z-index: 9999; color: #fff;">
+    <div class="close-video">
         <i class="fa fa-times-circle fa-lg"></i>
     </div>
     <iframe src="https://www.youtube.com/embed/ZCpfySvao9s?autoplay=0&amp;showinfo=0&amp;enablejsapi=1"
