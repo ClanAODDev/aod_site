2259e59b724a93135024a179cd60ee868b4bbbce
Chris Deaton <dcdeaton@ncsu.edu>
2019-05-13 10:13:01 -0400

    update FA, fix bullet styles

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <title>ClanAOD.net <?php echo ( ! is_front_page()) ? "| " . get_the_title() : ''; ?></title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/style.css?v=0.1.2" />
 
     <script src="<?= get_template_directory_uri() . '/assets/js/modernizr.js' ?>"></script>
