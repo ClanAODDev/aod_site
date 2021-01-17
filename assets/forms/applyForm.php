052648b0730f7d64934484cba48419ce30dcdb1e
Chris Deaton <dcdeaton@ncsu.edu>
2021-01-17 14:03:26 -0500

    check valid division in apply form

--- assets/forms/applyForm.php
+++ assets/forms/applyForm.php
@@ -9,15 +9,17 @@ $divisions = \ClanAOD\ExtensionsPlugin::allDivisions(); ?>
     <p>You must have a forum account in order to apply for one of our divisions.</p>
     <a href="/forums/register.php" target="_blank"
        class="call-to-action-button is-small">Create an account</a>
-    <hr class="margin-top-20" />
+    <hr class="margin-top-20"/>
     <h2 class="margin-top-20">2. Apply to a division</h2>
     <div class="games-listing">
         <?php foreach ($divisions as $post): setup_postdata($post); ?>
-            <a href="#" data-application-id="<?= meta('application_id') ?>"
-               data-application-link class="tooltips">
-                <img class="game" src="<?= meta('division_icon'); ?>"/>
-                <span><?= $post->post_title ?></span>
-            </a>
+            <?php if ($post): ?>
+                <a href="#" data-application-id="<?= meta('application_id') ?>"
+                   data-application-link class="tooltips">
+                    <img class="game" src="<?= meta('division_icon'); ?>"/>
+                    <span><?= $post->post_title ?></span>
+                </a>
+            <?php endif; ?>
         <?php endforeach;
         wp_reset_postdata(); ?>
     </div>
